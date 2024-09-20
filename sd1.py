6e85411047509eb83d117d164ae2713ab90317a4
hrishiareddy004 <169017796+hrishiareddy004@users.noreply.github.com>
2024-09-20 11:13:34 +0530

    sd1a.py

--- sd1.py
+++ sd1.py
@@ -2,3 +2,5 @@ Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("welcome to Jenkins");
 welcome to Jenkins
+print("Hello World");
+
