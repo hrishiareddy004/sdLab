78776069ffbda01838cccccd850894f3afc7a91a
hrishiareddy004 <169017796+hrishiareddy004@users.noreply.github.com>
2024-09-27 10:40:22 +0530

    Update sd1.py

--- sd1.py
+++ sd1.py
@@ -1,6 +1,3 @@
-Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("welcome to Jenkins");
-welcome to Jenkins
-print("Hello World");
+print("welcome to Jenkin")
+print("Hello World")
 
